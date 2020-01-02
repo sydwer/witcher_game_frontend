b26ec19187f7a7e790d1715fc5eea39111c58c76
Sydney Werbach <sydwer@gmail.com>
2020-01-02 12:25:38 -0700

    created basic home scrren/app.js with gif background and title

--- src/App.js
+++ src/App.js
@@ -1,26 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import axios from 'axios'
+import image from './something_witcher.png'
 
-function App() {
+import './App.css'
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <img src= {image} id= "image1"/>
     </div>
-  );
+  )
 }
 
-export default App;
+
